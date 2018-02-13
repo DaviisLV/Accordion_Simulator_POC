ce50920e7f173b05a1497c30ace7d97fba66eec6
DaviisLV <abols.davis@gmail.com>
2018-02-13 14:52:57 +0200

    first commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject RightArm;
+    public GameObject LeftArm;
+    int[,] array = new int[4, 3] { { 1,-4, 4 }, { 2,-2, 2 }, { 3,-1, 3 }, { 1,-4, 4 } };
+    int timeindex = 0;
+    bool corrutineRuning = false;
+// Use this for initialization
+    void Start () {
+
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GoTrueArray();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+  
+           
+        }
+        //left arm
+        if (Input.GetKey(KeyCode.Q))
+        {
+            MoveArmLeft(LeftArm, 10,-4);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            MoveArmRight(LeftArm,10,-1);
+        }
+        //right arm
+        if (Input.GetKey(KeyCode.I))
+        {
+            MoveArmLeft(RightArm, 10, 1);
+        }
+        if (Input.GetKey(KeyCode.P))
+        {
+            MoveArmRight(RightArm,10,4);
+        }
+    }
+
+    void GoTrueArray()
+    {
+        if (timeindex <= 3)
+        {
+            if (!corrutineRuning)
+                StartCoroutine(PositionSwicher(array[timeindex, 0]));
+        }
+        else
+        {
+            timeindex = 0;
+        }
+
+
+    }
+
+    IEnumerator PositionSwicher(int timeTillSwich)
+    {
+        corrutineRuning = true;
+        yield return new WaitForSeconds(timeTillSwich);
+        Debug.Log(timeindex+"/"+ timeTillSwich);
+        timeindex++;
+        corrutineRuning = false; 
+    }
+
+        #region arm movment
+        public void MoveArmLeft(GameObject arm,int moveSpeed,int stopPosition)
+    {
+        if (arm.transform.position.x > stopPosition)
+            arm.transform.position += Vector3.left * Time.deltaTime * moveSpeed;
+
+    }
+    public void MoveArmRight(GameObject arm, int moveSpeed, int stopPosition)
+    {
+        if (arm.transform.position.x < stopPosition)
+            arm.transform.position += Vector3.right * Time.deltaTime * moveSpeed;
+
+    }
+    #endregion
+}
