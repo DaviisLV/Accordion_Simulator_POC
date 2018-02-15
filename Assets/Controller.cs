a74afcead3eeec108503a241650cbadbd9659b52
DaviisLV <abols.davis@gmail.com>
2018-02-15 16:37:29 +0200

    add VR

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour {
 
+    public GameObject LeftConttroler;
+    public GameObject RighrtConntroler;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float dist = Vector3.Distance(LeftConttroler.transform.position, RighrtConntroler.transform.position);
+        Debug.Log(dist);
+    }
 }
