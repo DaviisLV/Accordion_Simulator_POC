5511ce8ada6a24245fd82bc9c77100bbbb73b743
DaviisLV <abols.davis@gmail.com>
2018-02-16 16:30:13 +0200

    add controller triger

---
+++ Assets/ControllerButtonPress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerButtonPress : MonoBehaviour {
+    private SteamVR_TrackedController device;
+
+    // Use this for initialization
+    void Start () {
+
+        device = GetComponent<SteamVR_TrackedController>();
+        device.TriggerClicked += Restart;
+
+	}
+	
+    void Restart(object sander, ClickedEventArgs e)
+    {
+        Debug.Log("Click");
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
