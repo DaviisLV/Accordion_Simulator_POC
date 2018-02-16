1cc288816d4e5775ae9981dc54c2853b5822b030
davis.abols <davis.abols@va.lv>
2018-02-16 09:33:31 +0200

    add curve scripts

---
+++ Assets/CurveScripts/BezierSpline.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System;
+
+public class BezierSpline : MonoBehaviour {
+
+   
+    static int V3 = 31;
+    public Vector3[] points = new Vector3[V3];
+    float pozX1 = 1;
+    float pozX2 = -1;
+    public void Awake()
+    {
+        GenerateV3();
+    }
+    public void GenerateV3()
+    {
+        float X = 0;
+        float Y = 0;
+        float Z = 0;
+        int rightSide = 1;
+        int leftSide = 0;
+        for (int i = 0; i < V3; i++)
+        {
+            if (rightSide <= 2)
+            {
+                X = pozX1;
+                rightSide++;
+                leftSide = 0;
+            }
+            else
+            {
+                if(leftSide <= 2)
+                {
+                    X = pozX2;
+                    leftSide++;
+                }
+                else
+                {
+                    rightSide = 0;
+                }
+            }
+            if ( i != 0)
+            {
+                Z = UnityEngine.Random.Range(Z, Z + 2f);
+            }
+            Debug.Log(i);
+            points[i] = new Vector3(X,Y,Z);
+        }
+    }
+
+
+public int CurveCount {
+		get {
+			return (points.Length - 1) / 3;
+		}
+	}
+
+	public Vector3 GetPoint (float t) {
+		int i;
+		if (t >= 1f) {
+			t = 1f;
+			i = points.Length - 4;
+		}
+		else {
+			t = Mathf.Clamp01(t) * CurveCount;
+			i = (int)t;
+			t -= i;
+			i *= 3;
+		}
+		return transform.TransformPoint(Bezier.GetPoint(points[i], points[i + 1], points[i + 2], points[i + 3], t));
+	}
+	
+	public Vector3 GetVelocity (float t) {
+		int i;
+		if (t >= 1f) {
+			t = 1f;
+			i = points.Length - 4;
+		}
+		else {
+			t = Mathf.Clamp01(t) * CurveCount;
+			i = (int)t;
+			t -= i;
+			i *= 3;
+		}
+		return transform.TransformPoint(Bezier.GetFirstDerivative(points[i], points[i + 1], points[i + 2], points[i + 3], t)) - transform.position;
+	}
+	
+	public Vector3 GetDirection (float t) {
+		return GetVelocity(t).normalized;
+	}
+
+}
