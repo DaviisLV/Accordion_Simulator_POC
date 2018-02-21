39cd3f6fe8bbea071370053a2fab0e6875ad0318
DaviisLV <abols.davis@gmail.com>
2018-02-21 12:00:52 +0200

    add body

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour {
 
-  public float poz = - 0.15f;
     public GameObject HeadCamera;
-    public GameObject PathHight;
+
 
 
     // Use this for initialization
     void Start()
     {
-        PathHight.transform.position = new Vector3(PathHight.transform.position.x, HeadCamera.transform.position.y + poz, PathHight.transform.position.z);
     }
 
 
         // Update is called once per frame
         void Update () {
+        this.gameObject.transform.position = HeadCamera.transform.position;
+
       
     }
 }
