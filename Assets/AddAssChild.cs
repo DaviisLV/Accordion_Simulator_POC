aaf4bf02d02b626d57239414eabcfe836551f1ad
DaviisLV <abols.davis@gmail.com>
2018-02-21 17:53:28 +0200

    pickUp Objects

--- Assets/AddAssChild.cs
+++ Assets/AddAssChild.cs
@@ -2,18 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AddAssChild : MonoBehaviour {
+public class PickUpObject : MonoBehaviour {
 
-    public Transform accardion;
+    private Valve.VR.EVRButtonId _triger = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
 
-	// Use this for initialization
-	void Start () {
+    private SteamVR_Controller.Device controller {
+        get {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+            }
+    }
+    private SteamVR_TrackedObject trackedObj;
 
-        accardion.SetParent(this.gameObject.transform);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private GameObject obj;
+    private FixedJoint fJoint;
+
+    void Start()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+        fJoint = GetComponent<FixedJoint>();
+    }
+
+    void Update()
+    {
+        if (controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (controller.GetPressDown(_triger))
+        {
+            PickUpObj();
+        }
+
+        if (controller.GetPressUp(_triger))
+        {
+            DropObj();
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Pickupable"))
+        {
+            obj = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        obj = null;
+    }
+
+    void PickUpObj()
+    {
+        if (obj != null)
+        {
+            fJoint.connectedBody = obj.GetComponent<Rigidbody>();
+        }
+        else
+        {
+            fJoint.connectedBody = null;
+        }
+    }
+
+    void DropObj()
+    {
+        if (fJoint.connectedBody != null)
+        {
+            fJoint.connectedBody = null;
+        }
+    }
 }
