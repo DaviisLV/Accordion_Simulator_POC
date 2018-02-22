e3cdf3dbf0f70ba2d048d1d603443273648c1c10
DaviisLV <abols.davis@gmail.com>
2018-02-22 11:14:44 +0200

    add accardion

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Controller : MonoBehaviour {
 
     public GameObject HeadCamera;
+    public GameObject Accardion;
 
 
 
     // Use this for initialization
     void Start()
     {
+        Accardion.transform.position = new Vector3(HeadCamera.transform.position.x, HeadCamera.transform.position.y - 1, HeadCamera.transform.position.z + 0.3f);
     }
 
 
