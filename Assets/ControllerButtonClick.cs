52ffd8c73c08a46c79120dee4fbf90bc6ea96250
DaviisLV <abols.davis@gmail.com>
2018-02-27 16:20:54 +0200

    add recorder

---
+++ Assets/ControllerButtonClick.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControllerButtonClick : MonoBehaviour {
+
+
+
+    private const Valve.VR.EVRButtonId Menu = Valve.VR.EVRButtonId.k_EButton_ApplicationMenu;
+    private const Valve.VR.EVRButtonId Triger = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    private SteamVR_TrackedObject _trackedObj;
+    private SteamVR_Controller.Device Controller { get { return SteamVR_Controller.Input((int)_trackedObj.index); } }
+
+    private void Start()
+    {
+        _trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+    }
+
+    private void Update()
+    {
+        if (Controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+        if (Controller.GetPressDown(Menu))
+        {
+            WriteV3InFile wr = new WriteV3InFile();
+            wr.StartRecord();    
+
+        }
+
+        if (Controller.GetPressDown(Triger))
+        {
+            WriteV3InFile wr = new WriteV3InFile();
+            wr.StopRecord();
+
+        }
+    }
+
+
+}
+
+
--- Assets/WriteV3InFile.cs
+++ Assets/WriteV3InFile.cs
@@ -7,13 +7,7 @@ using System.IO;
 
 public class WriteV3InFile : MonoBehaviour {
 
-    private const Valve.VR.EVRButtonId TriggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
-    private const Valve.VR.EVRButtonId GripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
-
-    private SteamVR_TrackedObject _trackedObj;
-    private SteamVR_Controller.Device Controllers { get { return SteamVR_Controller.Input((int)_trackedObj.index); } }
-
-
+    public AudioSource Song;
     public Transform Controller;
     public string FileName;
     string fileName;
@@ -27,31 +21,13 @@ public class WriteV3InFile : MonoBehaviour {
     }
     public void Start()
     {
-         _trackedObj = GetComponent<SteamVR_TrackedObject>();
 
-    }
-    public void StartRecord()
-    {
         if (File.Exists(fileName))
         Debug.Log(fileName + " already exists and will be owerrite.");
 
         FileStream fcreate = File.Open(fileName, FileMode.Create);
         sr = new StreamWriter(fcreate);
-            
-        StartCoroutine(Record());
-    }
 
-    private void Update()
-    {
-
-        if (Controllers.GetPressDown(TriggerButton))
-        {
-            StopRecord();
-        }
-        if (Controllers.GetPressDown(GripButton))
-        {
-            StartRecord();
-        }
     }
 
     public IEnumerator Record()
@@ -68,15 +44,23 @@ public class WriteV3InFile : MonoBehaviour {
     {
         return String.Format("{0:F4},{1:F4},{2:F4}", Controller.position.x, Controller.position.y, Controller.position.z);
     }
+   
+    void OnApplicationQuit()
+    {
+        StopRecord();
+    }
+
     public void StopRecord()
     {
         sr.Close();
         _isRecording = false;
+        Song.Stop();
         Debug.Log("Stop");
     }
-
-    void OnApplicationQuit()
+    public void StartRecord()
     {
-        StopRecord();
+        StartCoroutine(Record());
+        Song.Play();
+        Debug.Log("Start");
     }
 }
