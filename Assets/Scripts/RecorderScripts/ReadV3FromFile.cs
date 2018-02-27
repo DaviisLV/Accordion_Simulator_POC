dfd457dd19730c859ed02a7b2d60dc7ca02c5374
DaviisLV <abols.davis@gmail.com>
2018-02-27 18:07:46 +0200

    add recorder

--- Assets/Scripts/RecorderScripts/ReadV3FromFile.cs
+++ Assets/Scripts/RecorderScripts/ReadV3FromFile.cs
@@ -3,7 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ReadV3FromFile : MonoBehaviour {
+     public Transform items;
 
     public List<Vector3> points = new List<Vector3>();
+    Vector3[] allPoints;
 
-}
+
+    private void Awake()
+    {
+
+       allPoints = points.ToArray();
+    }
+
+  
+    private void Start()
+    {
+       
+        for (int  f = 0; f <= allPoints.Length; f++)
+        {
+           
+            Transform item = Instantiate(items) as Transform;
+            Vector3 position = allPoints[f];
+            item.transform.localPosition = position;
+              
+            }
+        }
+    }
+
+
