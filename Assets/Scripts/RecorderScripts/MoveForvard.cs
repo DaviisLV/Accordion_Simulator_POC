a02a2bb95d79a9187e0778455c6f58e8e1ad031d
DaviisLV <abols.davis@gmail.com>
2018-02-28 16:06:36 +0200

    bug fix on movment

--- Assets/Scripts/RecorderScripts/MoveForvard.cs
+++ Assets/Scripts/RecorderScripts/MoveForvard.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class MoveForvard : MonoBehaviour {
     public bool move = false;
-  
+    private Vector3 _startPoz;
+    private void Start()
+    {
+        _startPoz = this.transform.position;
+    }
+
     void Update () {
-		if ( move == true)
-        this.transform.position += Vector3.forward * Time.deltaTime * 2;
+        if (move == true)
+            this.transform.position += Vector3.forward * Time.deltaTime * 2;
+        else
+            this.transform.position = _startPoz;
+
     }
 }
