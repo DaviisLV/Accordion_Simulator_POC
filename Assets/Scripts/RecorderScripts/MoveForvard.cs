b518cc186816279d3f1fd82f3e505e4f054ee342
DaviisLV <abols.davis@gmail.com>
2018-02-28 11:50:21 +0200

    add new reader svript to main VR

--- Assets/Scripts/RecorderScripts/MoveForvard.cs
+++ Assets/Scripts/RecorderScripts/MoveForvard.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class MoveForvard : MonoBehaviour {
     public bool move = false;
-    public Transform Head;
-
-
-    private void Start()
-    {
-        this.transform.position = Head.position;
-    }
+  
     void Update () {
 		if ( move == true)
         this.transform.position += Vector3.forward * Time.deltaTime * 2;
